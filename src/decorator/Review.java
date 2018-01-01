0cc9e21ffb389966cd68c2902a494d10140d637a
Andre Darnius <andridarnius@gmail.com>
2018-01-01 18:04:29 +0700

    kommit

--- src/decorator/Review.java
+++ src/decorator/Review.java
@@ -1,12 +1,7 @@
 package decorator;
-public class Review {
+public abstract class Review {
     String nama = "";
     String comment = "";
-    Paket paket;
-
-    public Review(Paket paket) {
-        this.paket=paket;
-    }
 
     public String getReviews() {
         return null;
